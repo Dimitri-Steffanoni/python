987078d45b05b9791bee7c93835aa6194f61a4f0
user <dimitristeffanoni@gmail.com>
2022-12-07 17:18:41 +0100

    added schedule

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import schedule
+import time
 from datetime import datetime
 
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
+
+def job():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+#schedule.every(1).minutes.do(job)
+#schedule.every().hour.do(job)
+schedule.every().day.at("16:21").do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
